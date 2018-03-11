01bf0acfa0e01c3b4c38612d007828987033160a
unknown <sophdoiron@outlook.com>
2018-03-11 17:27:08 -0300

    fixed scorpion 6 gif and added footer component

--- project-blackmoor/src/app/app.module.ts
+++ project-blackmoor/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { StatsComponent } from '../app-stats.component/app-stats.component';
 import { BioComponent } from '../app-bio.component/app-bio.component';
 import { MonsterGeneratorService } from '../monster-generator/monster-generator.service';
 import { CHARACTER_GENERATOR } from '../shared/character-generator.injection-token';
+import { FooterComponent } from '../app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PictureComponent,
     BioComponent,
-    StatsComponent
+    StatsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
