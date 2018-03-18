24fbbf59377e584ce9c902bd87eb68f32d8aa25f
Soph <sophdoiron@outlook.com>
2018-03-18 13:53:31 -0300

    fixed the background

---
+++ project-blackmoor/src/app-header.component/app-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './app-header.component.html',
+  selector: 'app-header',
+  styleUrls: ['./app-header.component.css']
+})
+export class HeaderComponent {
+  public scroll(): void {
+
+  }
+}
